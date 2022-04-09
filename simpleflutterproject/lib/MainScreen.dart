67001f780d21c12d23273f73c571d510f1af5437
UrosSysPro <71965086+UrosSysPro@users.noreply.github.com>
2022-04-09 14:01:01 +0200

    Add files via upload

--- simpleflutterproject/lib/MainScreen.dart
+++ simpleflutterproject/lib/MainScreen.dart
@@ -12,7 +12,7 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   List<Task> tasks=[Task("operi ves",false)];
-  String _bottomSheetInputText="";
+  String _bottomSheetInputText="neki task";
   bool _bottomSheetSwitchValue=false;
 
   @override
@@ -40,36 +40,53 @@ class _MainScreenState extends State<MainScreen> {
         floatingActionButton: FloatingActionButton.extended(
           onPressed: (){
             showModalBottomSheet(context: context, builder: (context){
-              return Padding(
-                padding: const EdgeInsets.all(20.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    TextFormField(
-                      
-                      initialValue: "",
-                      decoration:InputDecoration(
-                        labelText: "Task Name",
-                        border: OutlineInputBorder()
+              return Container(
+                height: 300,
+                child: Padding(
+                  padding: const EdgeInsets.all(20.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      TextFormField(
+                        
+                        initialValue: "",
+                        onChanged: (value){
+                          _bottomSheetInputText=value;
+                        },
+                        decoration:InputDecoration(
+                          labelText: "Task Name",
+                          border: OutlineInputBorder()
+                        )
+                      ),
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: [ 
+                          Text("Done"),
+                          StatefulBuilder(
+                            builder: (context,setState) {
+                              return Switch(value: _bottomSheetSwitchValue,onChanged: (value){setState(() {
+                                _bottomSheetSwitchValue=value;
+                              });
+                              },);
+                            }
+                          )],
+                      ),
+                      Expanded(
+                          flex: 1,
+                          child: Column(
+                          
+                          crossAxisAlignment: CrossAxisAlignment.end,
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children:[
+                            ElevatedButton(onPressed: (){setState(() {
+                              tasks.add(Task(_bottomSheetInputText,_bottomSheetSwitchValue));
+                              Navigator.pop(context);
+                            });}, child: Text("Create"))
+                          ]
+                        ),
                       )
-                    ),
-                    Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [ 
-                        Text("Done"),
-                        Switch(value: _bottomSheetSwitchValue,onChanged: (value){setState(() {
-                          _bottomSheetSwitchValue=value;
-                        });
-                        },)],
-                    ),
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children:[
-                        ElevatedButton(onPressed: (){}, child: Text("Create"))
-                      ]
-                    )
-                  ],
+                    ],
+                  ),
                 ),
               );
             });
--- simpleflutterproject/lib/TasksTab.dart
+++ simpleflutterproject/lib/TasksTab.dart
@@ -1,27 +1,63 @@
 import 'package:flutter/material.dart';
 import 'package:simpleflutterproject/Task.dart';
 
-class TasksTab extends StatelessWidget {
+class TasksTab extends StatefulWidget {
   final List<Task> tasks;
 
   TasksTab(this.tasks);
+
+  @override
+  State<TasksTab> createState() => _TasksTabState();
+}
+
+class _TasksTabState extends State<TasksTab> {
+  double  offset=0;
+
+
   @override
   Widget build(BuildContext context) {
-    if(tasks.isEmpty){
+    if(widget.tasks.isEmpty){
       return Center(
         child:Text("No tasks, click + to add"),
       );
     }
 
+      
+
     return ListView(
-      children: tasks.map((task){
-        return Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-          Text(task.name),
-          Switch(onChanged: (value){},value: task.done,)
-        ],);
-      }).toList(),
+      children: widget.tasks.asMap().map((i, task) {
+        return MapEntry(i,
+          GestureDetector(
+              onHorizontalDragUpdate: (args){setState((){
+                offset+=args.delta.dx;
+              });
+              },
+              child: Stack(
+              children:[
+                Container(color: Colors.red,),
+                Transform.translate(
+                    offset: Offset(offset,0),
+                    child: Container(
+                    color:Colors.white,
+                    child:Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                      Text(task.name),
+                      Switch(
+                        value: task.done,
+                        onChanged:(value){setState((){
+                          widget.tasks[i].done=value;
+                        });
+                        }
+                      )
+                    ],)
+                  ),
+                )
+              ]
+            ),
+          )
+        );
+      }).values.toList(),
     );
   }
 }
